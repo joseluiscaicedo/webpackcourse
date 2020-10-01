4cd3331fb67761fcb06911e83fb00cf07d4589ef
jose caicedo <joseluiscl@gmail.com>
2020-10-01 06:18:14 -0500

    Versioning, cleaning and compressing files

--- platzi-badges/webpack.config.js
+++ platzi-badges/webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const TersetJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports={
   entry:{
@@ -10,9 +13,15 @@ module.exports={
   },
   output:{
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
+    filename: 'js/[name].[hash].js',
     publicPath: 'http://localhost:3001',
     chunkFilename: 'js/[id].[chunkhash].js'
+  },
+  optimization:{
+    minimizer: [
+      new TersetJSPlugin(),
+      new OptimizeCSSAssetsPlugin()
+    ]
   },
   module: {
     rules: [
@@ -26,7 +35,9 @@ module.exports={
         use: {
             loader: 'url-loader',
             options: {
-              limits: 9000,
+              limits: 1000,
+              name: '[hash].[ext]',
+              outputPath: 'assets'
             }
         }
       },
@@ -46,8 +57,8 @@ module.exports={
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-      chunkFilename:'css/[id].css'
+      filename: 'css/[name].[hash].css',
+      chunkFilename:'css/[id].[hash].css'
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html')
@@ -59,7 +70,10 @@ module.exports={
     new AddAssetHtmlPlugin ({
       filepath: path.resolve(__dirname, "dist/js/*.dll.js"),
       outputPath: 'js',
-      publicPath: 'http://localhost:3001',
+      publicPath: 'http://localhost:3001/js',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns:['**/app.*'],
     })
   ],
 }
